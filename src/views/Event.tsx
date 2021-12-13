31b4be323fcae9dcee3cb2139420feff3fb27454
shulaplai <75302349+shulaplai@users.noreply.github.com>
2021-12-13 19:19:38 +0800

    add text

--- src/views/Event.tsx
+++ src/views/Event.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import YouTube from "react-youtube";
+// import YouTube from "react-youtube";
 
 import { Footer } from "./Footer";
 import { Layout } from "./Layout";
@@ -12,7 +12,105 @@ function Event() {
       <div className="max-w-screen-lg mx-auto px-3">
         <div className="antialiased text-gray-600">
           <h1>Youtube Video</h1>
-          <YouTube videoId="BsINY1-d4Kg" />
+          <a>
+            Maxwell M. Scherzer...Native of Chesterfield, MO...Attended Parkway
+            Central High School and the University of Missouri...Named the Big
+            12 Conference Pitcher of the Year as a sophomore at the University
+            of Missouri in 2005 after going 9-4 with a 1.86 ERA in 16
+            starts...Pitched for Team USA in the Summer of 2005...Went 2-1 with
+            a 2.70 ERA and 24 strikeouts in four outings...He and his wife,
+            Erica, have two daughters, Brooklyn and Kacey...Polished veteran
+            right-hander, three-time Cy Young Award Winner and seven-time
+            All-Star signed a historic seven-year contract with Washington on
+            Jan. 21, 2015...Joined the Nationals after seven Major League
+            seasons with the Detroit Tigers (2010-14) and Arizona Diamondbacks
+            (2008-09)... Went 3-0 with a 2.40 ERA in six games (five starts) in
+            the 2019 Postseason...Made two starts in the 2019 World Series,
+            including a Game 7 start just days after a neck injury forced him to
+            miss his start in Game 5...Is one of just 10 pitchers in Major
+            League history to take home the Cy Young Award three or more times,
+            having claimed the 2013 American League Cy Young and the 2016 and
+            2017 National League awards...He joins Roger Clemens (7), HOF Randy
+            Johnson (5), HOF Steve Carlton (4), HOF Greg Maddux (4), Clayton
+            Kershaw (3), HOF Sandy Koufax (3), HOF Pedro Martinez (3), HOF Jim
+            Palmer (3) and HOF Tom Seaver (3) on that list...Hall of Famers
+            Gaylord Perry, Martinez, and Johnson, as well as Clemens and Roy
+            Halladay join Scherzer as the only pitchers to win the award in both
+            leagues...In his first two seasons in Washington, he became just the
+            fifth pitcher in MLB history to throw two no-hitters in the same
+            regular season (2015) and joined Hall of Famer Randy Johnson as the
+            only pitchers in MLB history to have a no-hitter and a 20-strikeout
+            game (2016) on their resume...Helped the Nationals become the first
+            team since the Milwaukee Braves (Aug. 18, 1960 - April 28, 1961) to
+            throw three no-hitters in a 162-game span (Jordan Zimmermann, Game
+            No. 162, 2014; Scherzer, Games No. 69 & 161, 2015). 2020 In his
+            sixth season with the Nationals, recorded his 11th consecutive
+            winning season... In 12 starts, he finished the 2020 season 5-4 with
+            a 3.64 ERA, the best mark among qualified Nationals pitchers...His
+            3.64 ERA was the best mark among qualified Nationals
+            pitchers...Ranked in the National League in strikeouts per 9.0
+            innings (3rd, 12.30), strikeouts (6th, 92) and strikeout-to-walk
+            ratio (8th, 4.00)...Struck out 21 batters across his first two
+            starts of the season...Struck out 11 on Opening Day, July 23 vs. New
+            York (AL)...Struck out 10 batters on July 29 vs. Toronto...He
+            recorded at least 10 strikeouts five times, tied for the most in the
+            National League with NYM Jacob deGrom and behind only CLE Shane
+            Bieber (8) for the most in Major League Baseball...Tossed 7.0
+            innings/0 ER twice (7/29 at TOR, 9/7 vs. TBR)...Passed Hall of Famer
+            Sandy Koufax with 98th 10+ strikeout game on Sept. 13 vs.
+            Atlanta...2020 Pitch Mix: Four Seam (46%), Slider (19%), Changeup
+            (16.1%), Cutter (9.7%), Curve (9.2%). 2019 Ranked in the National
+            League in strikeouts per 9.0 IP (1st, 12.69), strikeout-to-walk
+            ratio (1st, 7.36), strikeouts (3rd, 243), opp. OBP (4th, .266), WHIP
+            (4th, 1.03) and walks per 9.0 IP (4th, 1.72)...In 17 starts from May
+            22 through the end of the season, Scherzer went 9-2 with a 2.44 ERA,
+            156 strikeouts, 19 walks and a .201 opp. AVG...Entering that start
+            on May 22, he ranked 25th in the National League in ERA at 3.72...He
+            finished the season ranked sixth at 2.92...On May 26th he recorded
+            his 2,500th career strikeouts vs. San Diego (Margot)... Was named
+            National League Pitcher of the Month for June and National League
+            Player of the Week for the final week of the month...He went 6-0
+            with a 1.00 ERA (5 ER/45.0 IP) in six starts during the month...He
+            recorded 68 strikeouts against just five walks (13.6 SO/BB),
+            including a 24 strikeouts-to-zero-walks stretch spanning his last
+            two starts...Turned in his strongest pitching performance of the
+            year on June 2 to propel the Nationals to the 4-1 win and the series
+            win over the Cincinnati Reds...He struck out 15 batters in 8.0
+            innings of three-hit, one-run ball to secure his third win of the
+            season...It was the sixth time he struck out 15 or more batters in a
+            game, tying him with Sam McDowell for the sixth-most in Major League
+            Baseball history...Went 8-0 with a 1.86 ERA in 12 starts from June 2
+            to Sept. 8...Had 114 strikeouts, 13 walks and a .179 opp. AVG during
+            the winning streak... It tied for the longest streak in the season
+            by a Nationals player (Sánchez, 5/29-8/23) and tied for
+            third-longest streak in the National League in 019...The Nationals
+            won 11 of the 12 games...On Sept. 24th he recorded his 93rd, 10+
+            strikeout game of his career and his 11th of the season...Went 7-2
+            with a 2.64 ERA in 12 starts on the road this season... According to
+            FanGraphs.com, Scherzer led all Major League Baseball pitchers in
+            Fielding Independent Pitching (2.45) and ranked second in Wins Above
+            Replacement (6.5)...In the 2019 Postseason he went 3-0 with a 2.40
+            ERA in six games (five starts)...The Nationals went 6-0 when he
+            pitched...Opposing batters hit .193 with three doubles and four
+            homers against him...Right-handed batters were just 10-for-59 (.169)
+            with a double and two homers against him...Started Game 2 of the
+            NLCS against the Cardinals...Struck out 11 batters in 7.0 innings of
+            one-hit ball... Took a no-hitter into the seventh inning before
+            allowing a hit to Paul Goldschmidt...Joined Roger Clemens (2000 ALCS
+            Game 4) as the only pitchers in Postseason history to throw at least
+            7.0 innings with 11 strikeouts and one hit...According to the Elias
+            Sports Bureau, he has taken a no-hitter into the fifth inning in
+            five career Postseason starts, three more than the next closest
+            pitcher...Earned the win in Game 1 of the World Series, tossing 5.0
+            innings of two-run, five-hit ball...Became the 12th pitcher in World
+            Series history to amass at least seven punchouts while not pitching
+            more than 5.0 innings...According to the Elias Sports Bureau, with
+            his start against Zack Greinke, it marked the first time in World
+            Series history that a pair of Cy Young Award winners faced off in
+            Game 7 of the World Series...His 11.01 strikeouts per 9.0 innings in
+            his Postseason career ranked first in MLB history among pitchers
+            with at least 60.0 Postseason innings pitched.
+          </a>
         </div>
       </div>
 
--- src/views/News.tsx
+++ src/views/News.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable func-names */
 /* eslint-disable no-console */
 /* eslint-disable no-plusplus */
@@ -16,6 +17,50 @@ function News() {
       <div className="max-w-screen-lg mx-auto px-3">
         <div className="antialiased text-gray-600">
           <h1>Stats</h1>
+          <a>
+            Roberts told the announcers that “you can’t talk to let alone touch”
+            Scherzer during a start. Roberts shared that he patted Scherzer on
+            the behind after each inning of the pitcher’s first start with the
+            team. By the fourth inning, Scherzer had grown tired of it and
+            grumbled something under his breath. “Did he just say, ‘don’t effing
+            touch me?” Roberts said. Sure enough, that’s exactly what Scherzer
+            said. Roberts said he got the scoop from his players, who informed
+            the manager that Scherzer does not want to be touched during starts.
+            Later, after Scherzer completed his outing, Roberts asked him about
+            the “don’t f—ing touch me” line. Scherzer said indeed that was his
+            message to the manager and explained why. “This is my job,” Scherzer
+            told Roberts. “I don’t need any kind of congratulations or support.
+            This is my job.” So there you have it. Scherzer does not need the
+            positive reinforcement during his starts, and he does not like to be
+            touched. His methods definitely work. A 185-97 record with a career
+            3.18 ERA, three Cy Youngs and eight All-Star appearances say that
+            Scherzer knows what he’s doing. Roberts just got clued in.
+          </a>
+
+          <a>
+            Earlier this week, Washington Nationals ace Max Scherzer broke his
+            nose while (somewhat lazily) practicing his bunting, the latest
+            victory for the"universal DH" crowd. A less insane pitcher/human
+            probably would have at least skipped his next start, or even missed
+            more time in the increasingly careful new-world MLB, where players
+            miss a game if they have the sniffles. Mad Max? He was on the mound
+            two days later against the Philles, throwing seven scoreless
+            innings, striking out 10 batters, giving up just four hits and
+            notching his sixth victory of the season. He did get the nose
+            checked out, but only to satisfy team doctors. RELATED: Vanderbilt
+            pitcher Kumar Rocker just became your new favorite player “Trust me,
+            this thing looks a lot worse than it actually is,” Scherzer said
+            after the game. “I felt zero pain. There’s been plenty of other
+            injuries where I felt a lot of pain and I’ve had to pitch through.
+            I’ll hang my hat on those starts, but tonight I felt zero pain. This
+            is part of what you have to do. You take the ball every fifth time.
+            “That’s my responsibility to the team, to make sure I always post,
+            and I knew I could post tonight.” Funny thing is, it looked
+            incredibly painful:
+          </a>
+          <a>
+            https://www.youtube.com/watch?v=mxs6froLhrM&ab_channel=BrodSquadSports
+          </a>
         </div>
       </div>
       <div>
